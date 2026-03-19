0db1c881c9fe9a405111b796a81a2edc74587db9
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2026-03-19 12:11:30 +0700

    fix tailwind build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,4 @@
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
-export const metadata = {
-  title: "Daffa Audya P | Portfolio",
-  description: "Portfolio IT Programmer Daffa Audya",
-};
 
 export default function RootLayout({
   children,
@@ -13,12 +6,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="id">
-      <body className="bg-white text-gray-900 min-h-screen">
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
